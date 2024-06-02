0b1e6d32ce5b8673fc20a2f8a7b922892fc4fb2a
HB01480 <nazmulphy@gmail.com>
2024-06-02 14:12:36 +0600

    Added Main Function Boilerplate code

--- src/main.cc
+++ src/main.cc
@@ -2,6 +2,30 @@
 
 
 int main() {
+    // Initialization
+    InitWindow(SC_WIDTH, SC_HEIGHT, TITLE);
+    SetWindowState(FLAG_VSYNC_HINT);
+
+    // Game initialization
+
+
+    // Game loop
+    while (!WindowShouldClose()) {
+        // Updating
+
+
+        // Drawing
+        BeginDrawing();
+
+        ClearBackground(BG_COLOR);
+        
+        EndDrawing();
+
+    }
+
+    // Deinitialization
+    CloseWindow();
 
     return EXIT_SUCCESS;
+
 }
